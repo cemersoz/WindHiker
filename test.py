4c59571640ca14d0cb90e2c82c23a6f53a8fcdc9
Mert Bozfakioglu <mertbozfakioglu@yahoo.com>
2017-11-07 14:11:36 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import database
-
-dat = database.GFS()
+import pygrib 
+gfs = database.GFS()
 #dat.download_latest()
-print(dat.get_date())
+gfs.load('latest.grb2')
+grb = gfs.files[-1].select(name='Maximum temperature')[0]
+nebu = (grb.data()[1][360][719])
+print(nebu)
+print(gfs.get_date())
