84a23c631570fba6f84650c2041cccd6e29c83d5
Mert Bozfakioglu <mertbozfakioglu@yahoo.com>
2017-11-07 14:11:12 -0500

    Update database.py

--- database.py
+++ database.py
@@ -4,13 +4,15 @@ import time
 
 class Database(object):
     def __init__(self):
+        self.files = []
         pass
 
     def download(url,file_name):
         os.system("curl %s -o %s" % (url,file_name))
 
-    def open_grib(file_name):
-        return pygrib.open(file_name)
+    def load(self,file_name):
+        self.files.append(pygrib.open(file_name))
+        return self.files[-1]
 
     def get_date(self):
         #year,month,day
@@ -21,6 +23,6 @@ class GFS(Database):
     def __init__(self):
         Database.__init__(self)
 
-    def download_latest(self):
+    def download_latest(self,name="latest.grb2"):
         #random url
-        Database.download("https://nomads.ncdc.noaa.gov/data/gfsanl/201711/20171104/gfsanl_4_20171104_0000_006.grb2","test.grb2")
+        Database.download("https://nomads.ncdc.noaa.gov/data/gfsanl/201711/20171104/gfsanl_4_20171104_0000_006.grb2",name)
