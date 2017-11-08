5405d0932a46b064d7bf1db7c0c9f60a76d3f490
Mert Bozfakioglu <mertbozfakioglu@yahoo.com>
2017-11-07 19:00:27 -0500

    added wind data field querry

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import database
 import pygrib 
+import numpy
+
+
 gfs = database.GFS()
 #dat.download_latest()
 gfs.load('latest.grb2')
-grb = gfs.files[-1].select(name='Maximum temperature')[0]
-nebu = (grb.data()[1][360][719])
-print(nebu)
+max_temperatures = gfs.file.select(name='Maximum temperature')[0]
+deneme = gfs.file[3]
+gfs.init_wind_data()
+print(gfs.wind_data)
+#for e in gfs.file:
+#    print(e)
+max_temperatures_2d_array = (max_temperatures.values)
+print(max_temperatures_2d_array.shape)
+#numpy.savetxt("max_temperatures_2d_array.csv", max_temperatures_2d_array, delimiter=",")
+#print(gfs.file)
 print(gfs.get_date())
+#gfs.init_wind_data()
